0356f2f52dcda4628842971f6effbad95416f715
Andrea Testa <andrea.testa7@unibo.it>
2022-06-28 10:55:30 +0200

    AT create ros2 package

---
+++ crazychoir/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'crazychoir'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='andrea',
+    maintainer_email='andrea.testa7@unibo.it',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
