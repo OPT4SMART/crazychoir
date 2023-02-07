f3170e59e84f7b812c15ddb9f1ae9078a8e688a0
lorenzopichierri <lorenzopichierri@gmail.com>
2023-02-07 17:26:56 +0100

    LP example: update rviz formation

--- crazychoir_examples/crazychoir_examples/bearingformationcontrol/controller.py
+++ crazychoir_examples/crazychoir_examples/bearingformationcontrol/controller.py
@@ -6,13 +6,14 @@ from crazychoir.crazyflie_communication import FPQRSender
 def main():
     rclpy.init()
 
+    vicon = False
     frequency = 100
-    position_ctrl = FlatnessAccelerationCtrl(frequency)
-    attitude_ctrl = GeometryAttitudeCtrl(frequency)
+    position_ctrl = FlatnessAccelerationCtrl(vicon, frequency)
+    attitude_ctrl = GeometryAttitudeCtrl(vicon, frequency)
     sender = FPQRSender(frequency)
     desired_acceleration = AccelerationTraj()
 
-    controller = HierarchicalController(pos_handler='pubsub', pos_topic='odom', 
+    controller = HierarchicalController(pose_handler='pubsub', pose_topic='odom', 
                 position_strategy=position_ctrl, attitude_strategy=attitude_ctrl, 
                 command_sender=sender, traj_handler=desired_acceleration)
 
