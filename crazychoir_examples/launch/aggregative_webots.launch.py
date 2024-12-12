f8ba5135d66efbdceaf67ac1c1fcf3cee61ad45b
Lorenzo Pichierri <85987969+lorenzopichierri@users.noreply.github.com>
2024-12-12 10:37:56 +0100

    LP - Fixed typo in aggregative_webots.launch.py

--- crazychoir_examples/launch/aggregative_webots.launch.py
+++ crazychoir_examples/launch/aggregative_webots.launch.py
@@ -105,7 +105,7 @@ def generate_launch_description():
     # Launch control Panel
     launch_description.append(Node(
                 package='crazychoir_examples', 
-                executable='crazychoir_aggregative_lighthouse_gui',
+                executable='crazychoir_aggregative_webots_gui',
                 output='screen',
                 parameters=[{
                     'n_agents': N,
